d740afc92fb7c555c4f65ccf61ec58e629c721bf
ZachVerdin <66070441+ZachVerdin@users.noreply.github.com>
2020-05-28 08:44:24 -0400

    Update programs.js

--- app/data/programs.js
+++ app/data/programs.js
@@ -2,7 +2,7 @@ export default [
 	{
 		title: 'Knowledge Ecosystems',
 		description: 'We examine how knowledge ecosystems exist today, develop playbooks for how to improve them, and facilitate new knowledge life cycles accelerated through multi-institution collaboration.',
-		focusAreas: ['Education', 'Neo-liberalism', 'Justice', 'Equity'],
+		focusAreas: ['Education', 'Neo-liberalism', 'Justice', 'Inclusion'],
 	},
 	{
 		title: 'Community Publishing',
