ae67920fff5c008adeb3a53fb6e1041df3e03494
Travis Rich <travis.s.rich@gmail.com>
2020-07-02 17:07:46 +0100

    update: Add Editorial job

--- app/data/jobs.js
+++ app/data/jobs.js
@@ -1,16 +1,22 @@
 export default [
 	{
-		title: 'Software Engineer, PubPub',
+		title: 'Editorial Manager, PubPub',
 		location: 'Cambridge, MA or Remote',
 		commitment: 'Full-time',
-		link: 'https://notes.knowledgefutures.org/pub/rh7vxl1u',
+		link: 'https://notes.knowledgefutures.org/pub/f7dqq3y2',
 	},
-	{
-		title: 'Junior Software Engineer, PubPub',
-		location: 'Cambridge, MA or Remote',
-		commitment: 'Full-time',
-		link: 'https://notes.knowledgefutures.org/pub/8ybcdqbn',
-	},
+	// {
+	// 	title: 'Software Engineer, PubPub',
+	// 	location: 'Cambridge, MA or Remote',
+	// 	commitment: 'Full-time',
+	// 	link: 'https://notes.knowledgefutures.org/pub/rh7vxl1u',
+	// },
+	// {
+	// 	title: 'Junior Software Engineer, PubPub',
+	// 	location: 'Cambridge, MA or Remote',
+	// 	commitment: 'Full-time',
+	// 	link: 'https://notes.knowledgefutures.org/pub/8ybcdqbn',
+	// },
 	{
 		title: 'Software Engineer, the Underlay Project',
 		location: 'Cambridge, MA or Remote',
