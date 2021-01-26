088a71a06f765346af48a4c363a74473fe86ade9
Gabriel Stein <g@gabestein.com>
2021-01-26 09:55:31 -0500

    update: Add PubPub job

--- app/data/jobs.js
+++ app/data/jobs.js
@@ -12,11 +12,11 @@ export default [
 	// 	link: 'https://notes.knowledgefutures.org/pub/rh7vxl1u',
 	// },
 	// {
-	// 	title: 'Junior Software Engineer, PubPub',
-	// 	location: 'Cambridge, MA or Remote',
-	// 	commitment: 'Full-time',
-	// 	link: 'https://notes.knowledgefutures.org/pub/8ybcdqbn',
-	// },
+	 	title: 'Software Engineer, PubPub',
+	 	location: 'Cambridge, MA or Remote',
+	 	commitment: 'Full-time',
+	 	link: 'https://notes.knowledgefutures.org/pub/rh7vxl1u/',
+	 },
 	{
 		title: 'Software Engineer, the Underlay Project',
 		location: 'Cambridge, MA or Remote',
