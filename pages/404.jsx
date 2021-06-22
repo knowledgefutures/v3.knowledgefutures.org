f27df08cfda3c1615af4e0874c15f65d60206a58
Travis Rich <travis.s.rich@gmail.com>
2021-06-22 16:36:58 -0400

    typos and update

---
+++ pages/404.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+import styles from "./404.module.scss";
+
+export default function NotFound() {
+	return (
+		<div className={styles.notFound}>
+			<Head>
+				<title>404 Not Found · Knowledge Futures Group</title>
+			</Head>
+			<h1>Page Not Found</h1>
+			<img src="/images/404.svg" />
+		</div>
+	);
+}
