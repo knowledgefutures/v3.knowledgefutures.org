39486a3e54c1da9034669bf6ea6c806c7911f7bd
Gabriel Stein <g@gabestein.com>
2021-08-27 14:17:10 -0400

    limit to 4 feed items for now

--- components/NewsList/NewsList.jsx
+++ components/NewsList/NewsList.jsx
@@ -4,18 +4,21 @@ import styles from "./NewsList.module.scss";
 
 import kfnotes from "./kfnotes";
 
+const limit = 4;
 const news = kfnotes.rss.channel.item;
 
 const NewsList = function () {
 	return (
 		<ul className={styles.news}>
-			{news.map((news) => {
-				const { link, title, description, image } = news;
-				return (
-					<li key={link}>
-								<a className="hoverline" href={link}>{title}</a>
-					</li>
-				);
+			{news.map((news, i) => {
+				if (i < 4) {
+					const { link, title, description, image } = news;
+					return (
+						<li key={link}>
+									<a className="hoverline" href={link}>{title}</a>
+						</li>
+					);
+				}
 			})}
 		</ul>
 	);
