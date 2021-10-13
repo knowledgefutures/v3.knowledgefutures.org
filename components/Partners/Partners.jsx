0f0440d509ce7ddeb4b071a417b4a68d2de064eb
Travis Rich <travis.s.rich@gmail.com>
2021-10-13 15:43:41 -0400

    Add mcgovern - doh

--- components/Partners/Partners.jsx
+++ components/Partners/Partners.jsx
@@ -3,19 +3,19 @@ import styles from "./Partners.module.scss";
 const Partners = () => {
 	const partners = [
 		{
-			title: 'MIT Press',
-			img: '/partners/mitp.png',
-			link: 'https://mitpress.mit.edu/',
+			title: "MIT Press",
+			img: "/partners/mitp.png",
+			link: "https://mitpress.mit.edu/",
 		},
 		{
-			title: 'MacArthur Foundation',
-			img: '/partners/macarthur.png',
-			link: 'https://www.macfound.org',
+			title: "MacArthur Foundation",
+			img: "/partners/macarthur.png",
+			link: "https://www.macfound.org",
 		},
 		{
-			title: 'Reid Hoffman',
-			img: '/partners/reid.png',
-			link: 'https://www.reidhoffman.org/',
+			title: "Reid Hoffman",
+			img: "/partners/reid.png",
+			link: "https://www.reidhoffman.org/",
 		},
 		// {
 		// 	title: 'Goldsmiths Press',
@@ -23,23 +23,27 @@ const Partners = () => {
 		// 	link: 'https://www.gold.ac.uk/goldsmiths-press/',
 		// },
 		{
-			title: 'Sloan Foundation',
-			img: '/partners/sloan.png',
-			link: 'https://sloan.org/',
+			title: "Sloan Foundation",
+			img: "/partners/sloan.png",
+			link: "https://sloan.org/",
 		},
 		{
-			title: 'Siegel Family Foundation',
-			img: '/partners/siegel.png',
-			link: 'https://www.siegelendowment.org/',
+			title: "Siegel Family Foundation",
+			img: "/partners/siegel.png",
+			link: "https://www.siegelendowment.org/",
 		},
 		{
-			title: 'Protocol Labs',
-			img: '/partners/proto.png',
-			link: 'https://protocol.ai/',
+			title: "Protocol Labs",
+			img: "/partners/proto.png",
+			link: "https://protocol.ai/",
 		},
 
 		// {title: 'Lever for Change', img: '/partners/lever.png', link: 'https://www.leverforchange.org/' },
-		// {title: 'Patrick J. McGovern Foundation', img: '/partners/mcgovern.png', link: 'https://www.mcgovern.org/' },
+		{
+			title: "Patrick J. McGovern Foundation",
+			img: "/partners/mcgovern.png",
+			link: "https://www.mcgovern.org/",
+		},
 	];
 
 	return (
