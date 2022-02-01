166c2ba8a777dd21ab67d9447bd68008497f0efc
Travis Rich <travis.s.rich@gmail.com>
2022-02-01 18:07:55 -0500

    update members

--- utils/members.js
+++ utils/members.js
@@ -4,5 +4,8 @@ export const contributing = [
 	"Media Studies Press",
 	"San Antonio Review",
 	"MIT Libraries",
+	"Iowa State University Library",
+	"Vanderbilt University Jean & Alexander Heard Libraries",
+	"KU Leuven Libraries",
 ];
-export const supporting = [];
+export const supporting = ["Appalachian State University Belk Library and Information Commons"];
